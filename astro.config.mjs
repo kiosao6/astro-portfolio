63c30c7379d6f25f50c8545d34f82c9579d5554d
Gabriel <gabrielmaestre79@gmail.com>
2024-05-22 23:00:59 -0400

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 import { defineConfig } from 'astro/config';
-
+import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+import {
+  transformerNotationDiff,
+  transformerNotationFocus,
+  transformerMetaHighlight
+} from '@shikijs/transformers'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'catppuccin-macchiato',
+      transformers: [
+        transformerNotationDiff(),
+        transformerNotationFocus(),
+        transformerMetaHighlight(),
+      ],
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"lila": "#DCD7FF"
+			},
+			animation: {
+        marquee: 'marquee 45s linear infinite',
+        marquee2: 'marquee2 45s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+		},
 	},
 	plugins: [],
 }
