f0212a40b047864913b10e5aa35e86c9c2aa072c
Gabriel <gabrielmaestre79@gmail.com>
2024-05-22 23:07:27 -0400

    third commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,9 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
-import {
-  transformerNotationDiff,
-  transformerNotationFocus,
-  transformerMetaHighlight
-} from '@shikijs/transformers'
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  markdown: {
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'catppuccin-macchiato',
-      transformers: [
-        transformerNotationDiff(),
-        transformerNotationFocus(),
-        transformerMetaHighlight(),
-      ],
-    },
-  },
+  integrations: [tailwind(), react()]
 });
